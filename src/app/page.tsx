9e0261b7918e1adce5712da2b2fa308a265d79b2
Kunal Singh <ks820177@gmail.com>
2024-06-28 19:04:49 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen m-5 font-sans">
-      <div className="absolute flex justify-center itcems-center gap-10 h-full w-full bg-red-400 rounded-[75px]">
-        <div className="my-auto text-end w-[25%] space-y-5">
+    <div className="relative min-h-screen m-5 font-sans max-lg:flex max-lg:flex-col max-lg:bg-red-400 max-lg:rounded-[40px] max-lg:justify-center max-lg:items-center">
+      <div className="lg:absolute flex justify-center max-lg:my-auto items-center gap-10 h-full w-full bg-red-400 md:rounded-[75px]">
+        <div className="my-auto text-center lg:text-end lg:w-[25%] space-y-5">
           <p className="text-3xl font-black text-white">Witt.</p>
           <p className="text-7xl font-black text-white">Your second brain</p>
           <p className="text-white font-medium">
@@ -16,10 +16,10 @@ export default function Home() {
           alt="social"
           height={400}
           width={400}
-          className="flex justify-start items-start"
+          className="justify-start items-start hidden lg:flex"
         />
       </div>
-      <div className="absolute  left-0 m-10 bg-red-300 h-56 w-56 rounded-[65px] z-20">
+      <div className="absolute hidden lg:block left-0 m-10 bg-red-300 h-56 w-56 rounded-[65px] z-20">
         <Image
           src={"/travel.svg"}
           height={150}
@@ -28,7 +28,7 @@ export default function Home() {
           className="flex justify-start items-center w-full mt-10"
         />
       </div>
-      <div className="absolute right-0 m-10 bg-black h-20 w-20 rounded-full z-20">
+      <div className="absolute hidden lg:block right-0 m-10 bg-black h-20 w-20 rounded-full z-20">
         <Image
           src={"/play.svg"}
           height={30}
@@ -37,7 +37,7 @@ export default function Home() {
           className="flex justify-start items-center mx-7 my-5"
         />
       </div>
-      <div className="absolute flex justify-center items-center  text-white left-0 m-10 bottom-0 bg-black h-16 w-56 rounded-full z-20">
+      <div className="lg:absolute flex justify-center max-lg:my-auto items-center text-white lg:left-0 lg:m-10 lg:bottom-0 bg-black h-16 w-56 rounded-full z-20">
         Download For Free
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,49 @@
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 function Navbar() {
   return (
     <div className="flex justify-between items-center m-5">
       <div>
         <p className="text-red-500 text-3xl font-black font-sans">Witt</p>
       </div>
-      <div className="flex justify-start items-start gap-10 mx-auto font-medium">
-        <p className="cursor-pointer">Home</p>
-        <p className="cursor-pointer">Pricing</p>
-        <p className="cursor-pointer">Blog</p>
-        <p className="cursor-pointer">FAQs</p>
-        <p className="cursor-pointer">About Us</p>
+      <div className=" hidden md:flex justify-start items-start gap-10 mx-auto font-medium">
+        <Link href={"#"}>Home</Link>
+        <Link href={"#"}>Pricing</Link>
+        <Link href={"#"}>Blog</Link>
+        <Link href={"#"}>FAQs</Link>
+        <Link href={"#"}>About Us</Link>
+      </div>
+      <div className="md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="bg-red-400 text-white font-medium rounded-xl px-4 py-2 focus:outline-none mr-4">
+            Menu
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Link href={"#"}>Home</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={"#"}>Pricing</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={"#"}>Blog</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={"#"}>FAQs</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={"#"}>About Us</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
